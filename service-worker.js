297e6e44d5a2e0a568c5f7b57c307c6b178f4616
Sutthisak Kawaguchi <your-email@example.com>
2026-04-23 12:45:22 +0700

    up api

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'survey-pro-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './manifest.json',
+    './icon.png'
+];
+
+self.addEventListener('install', event => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => response || fetch(event.request))
+    );
+});
