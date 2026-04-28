4796598fbc7495acd2df1b289c3e91b385224633
Sutthisak Kawaguchi <your-email@example.com>
2026-04-28 22:04:53 +0700

    up24

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
     // Supabase Configuration
     SUPABASE_URL: 'https://vznbkqbmysinxtspsskl.supabase.co/rest/v1/',
-    SUPABASE_KEY: 'sb_publishable_xuIHGSlxLMB8b_nQ0nRpTA_yCSS5Px-',
+    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ6bmJrcWJteXNpbnh0c3Bzc2tsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcwMjcyMTYsImV4cCI6MjA5MjYwMzIxNn0.mF7LRqXEg1KP1QL1seEx4wFlmx978WaS6u4jWETg_PQ',
 
     // GAS_URL (Optional if switching fully to Supabase)
     //GAS_URL: 'https://script.google.com/macros/s/AKfycbyMKd_ebvJcGP77ap72Qobofr97blpY-xMiYnzoi8iyNkPZlsHuhdrwsyuF7Qu7Yxkb/exec',
